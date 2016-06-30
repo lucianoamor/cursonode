35628f7d0aed467ccc53395969160356bb2da7f3
LA <lucianoamor@gmail.com>
2016-06-30 18:44:41 -0300

    init

---
+++ c2/lib/server.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const publicDirectory = process.cwd()+"/public";
+
+var dinamicRoutes={
+	'ruta1': function() { return 1}
+};
+
+function Request(req, res){
+	this.req = req;
+	this.res = res;
+ 	console.log(req.url);
+  
+  if(!this.staticFile()){
+  	if(!this.dinamicFile()){
+  		this.notFound();
+  	}
+  }
+}
+Request.prototype.staticFile = function(){
+	console.log("is static?");
+	var _this = this;
+	let file="";
+  var content="";
+  if(_this.req.url=="/") file="/index.html";
+  try{
+  	content = fs.readFileSync(publicDirectory+file);
+  	return _this.res.end(content);
+  }catch(e){
+  	return null;
+  }
+ 
+}
+
+Request.prototype.dinamicFile = function(){
+	console.log("is dinamic?");
+	var _this = this;
+	try{
+  		content = server.get(_this.req.url, );
+  		return _this.res.end(content);
+  	}catch(e){
+	  	return null;
+	  }
+}
+
+Request.prototype.notFound = function(){
+	console.log("404");
+	this.res.end(fs.readFileSync(publicDirectory+"/404.html"));
+}
+
+function server(){
+
+	console.log("new server");
+	this.Request = function(req, res){
+		console.log("new request");
+		new Request(req, res);
+	}
+}
+
+server.prototype.get = function(path, cb){
+	dinamicRoutes[path] = cb;
+}
+module.exports = server;
